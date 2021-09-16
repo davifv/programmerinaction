6457799a742565cd88f057e0c9495f107154419d
davifv <90867520+davifv@users.noreply.github.com>
2021-09-16 16:25:19 -0300

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,12 @@
+    function goTo(element, speed) {
+  var href = element.attr('href');
+  var top = $(href).offset().top;
+  $("html,body").animate({scrollTop : top}, speed);
+}
+
+$(function() {
+  $("#top a").click(function(e) {
+    e.preventDefault();
+    goTo($(this), 900);
+  });
+});
